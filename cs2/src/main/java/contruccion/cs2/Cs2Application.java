9011ab8382ca43b321950c4e38d50091fbbecf14
juanjimenezg81 <juan.jimenez81@correo.tdea.edu.com>
2026-02-18 21:26:41 -0500

    Initial commit proyecto CS2

---
+++ cs2/src/main/java/contruccion/cs2/Cs2Application.java
@@ -0,0 +1,13 @@
+package contruccion.cs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs2Application.class, args);
+	}
+
+}
---
+++ cs2/src/test/java/contruccion/cs2/Cs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package contruccion.cs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
